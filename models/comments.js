5da0c5c82568f490ea0760bc6552c5eaf5e1cb98
Ned Vogel <nedvogel@Neds-MacBook-Air.local>
2018-07-12 23:48:50 -0700

    homework commit

---
+++ models/comments.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+
+var Schema = mongoose.Schema;
+
+
+var commentSchema = new Schema({
+
+  title: String,
+
+  body: String
+});
+
+
+var comment = mongoose.model("comment", commentSchema);
+
+
+module.exports = comment;
